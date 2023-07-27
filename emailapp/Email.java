22394da7ba620aeb5697a4277c9f7357a8eaf28e
annk1995 <kabuikariuki052@gmail.com>
2023-07-27 15:37:52 +0300

    first commit

---
+++ emailapp/Email.java
@@ -0,0 +1,16 @@
+package emailapp;
+
+public class Email {
+   private String  firstName;
+   private  String lastName;
+   private String password;
+   private String  department;
+   private int  mailboxCapacity;
+  private  String alternateEmail;
+   //constructor to recieve firt name and last name
+   //ask fordepartment
+   //generate randompassword
+   //set the mailbox capacity
+   //set alteernate email
+   //change password
+}
